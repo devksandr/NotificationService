49667918c523ab6241e5c8ec3fca6af5c8946d62
Ksandr <MYEMAIL>
2024-09-10 19:09:17 +0700

    Installed Hangfire and Hangfire.Storage.SQLite

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,17 +1,21 @@
+using Hangfire;
+using Hangfire.Storage.SQLite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHangfire(configuration => configuration
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSQLiteStorage());
+builder.Services.AddHangfireServer();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseHangfireDashboard("/dashboard");
 app.Run();
